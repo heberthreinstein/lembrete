8a959bef114dd2bc2985f415cffcf1c5d1c7821d
Heberth Reinstein <heberth.reinstein@gmail.com>
2019-08-27 20:33:17 -0300

    init]

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'inicio', pathMatch: 'full' },
+  { path: 'inicio', loadChildren: './pages/inicio/inicio.module#InicioPageModule' },
+  { path: 'cadastro', loadChildren: './pages/cadastro/cadastro.module#CadastroPageModule' },
+  { path: 'listar', loadChildren: './pages/listar/listar.module#ListarPageModule' },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/pages/cadastro/cadastro.page.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { Route } from '@angular/compiler/src/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-cadastro',
+  templateUrl: './cadastro.page.html',
+  styleUrls: ['./cadastro.page.scss'],
+})
+export class CadastroPage implements OnInit {
+
+  lembrete: {
+    titulo: string,
+    descricao: string,
+    data
+  };
+
+  constructor(private alertCtrl: AlertController,
+              private route: Router) { }
+
+  ngOnInit() {
+  }
+
+  public async cadastrar() {
+    let alert = await this.alertCtrl.create({
+      header: 'Não se preocupe',
+      message: 'Não deixaremos você esquecer',
+      buttons : [{
+        text: 'Obrigado!',
+        handler: () => {
+          this.route.navigate(['inicio'], {
+            queryParams: this.lembrete
+          });
+        }
+      }]
+    });
+
+    await alert.present();
+
+  }
+
+}

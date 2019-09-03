c0cfee8c6540f3bbced5792142018389eb17c779
Heberth Reinstein <heberth.reinstein@gmail.com>
2019-09-02 22:29:23 -0300

    CEP

--- src/app/pages/cep/cep.page.ts
+++ src/app/pages/cep/cep.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CepService } from 'src/app/services/cep.service';
+import { ToastController } from '@ionic/angular';
+import { async } from '@angular/core/testing';
 
 @Component({
   selector: 'app-cep',
@@ -11,11 +13,22 @@ export class CepPage implements OnInit {
   cep: string;
   result: any;
 
-  constructor(private cepService: CepService) { }
+  constructor(private cepService: CepService, private t: ToastController) { }
 
   buscaCep(cep){
     this.cepService.buscaCep(this.cep)
-    .subscribe(result => (this.result = result));
+    .subscribe(result => (this.result = result),
+    error => this.showError(error.message));
+  }
+
+  
+    public async showError(msg: any){
+      const t = this.t.create({
+      message: msg,
+      duration: 3000,
+      closeButtonText: 'Ok',
+      showCloseButton: true
+      });
   }
 
   ngOnInit() {
